c366515449621a8aed84a98c6185514d167316a3
Anandha Sudhan <rasudhan@gmail.com>
2018-10-04 22:59:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function viewCart() {
   // write your code here
   var items=[]
   for(let i=0;i<cart.length;i++) {
-    items.push(cart[i]['itemName']+"at $"+cart[i]['itemPrice']);
+    items.push(cart[i]['itemName']+" at $"+cart[i]['itemPrice']);
   }
   return `In your cart, you have ${items.join(', ')}.`
 }
